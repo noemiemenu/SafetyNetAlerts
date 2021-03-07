d922cb1f950e026e7b42cc08b025890409c54e23
Noémie Menu <71379054+noemiemenu@users.noreply.github.com>
2021-03-07 15:35:42 +0100

    - create endpoint : http://localhost:8080/firestation?stationNumber=<station_number> (#6)

--- src/main/java/com/safetynet/alerts/model/Person.java
+++ src/main/java/com/safetynet/alerts/model/Person.java
@@ -1,30 +1,43 @@
 package com.safetynet.alerts.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Table(name = "persons")
 public class Person {
 
     @Id
+    @JsonIgnore
     @GeneratedValue
     private Integer id;
+
     private String firstName;
+
     private String lastName;
+
     private String address;
+
     private String city;
+
     private String zip;
+
     private String phone;
+
     private String email;
+
+    @Transient
+    @JsonIgnore
+    private boolean isChild;
 
     @Override
     public String toString() {
--- src/main/java/com/safetynet/alerts/repositories/PersonsRepository.java
+++ src/main/java/com/safetynet/alerts/repositories/PersonsRepository.java
@@ -1,8 +1,13 @@
 package com.safetynet.alerts.repositories;
 
 import com.safetynet.alerts.model.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonsRepository extends CrudRepository<Person, Integer> {
+import java.util.List;
+import java.util.Set;
 
+public interface PersonsRepository extends CrudRepository<Person, Integer> {
+    @Query("select person from Person person where person.address in :addresses")
+    List<Person> getPeopleByAddresses(Set<String> addresses);
 }

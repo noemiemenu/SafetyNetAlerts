d0843899ec222de56be7dd785605a2db800dddc9
Noemie Menu <menunoemie.dev@outlook.com>
2021-03-13 18:12:17 +0100

    - create endpoint : http://localhost:8080/childAlert?address=<address>

--- src/main/java/com/safetynet/alerts/service/FirestationService.java
+++ src/main/java/com/safetynet/alerts/service/FirestationService.java
@@ -1,7 +1,6 @@
 package com.safetynet.alerts.service;
 
 import com.safetynet.alerts.model.Firestation;
-import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.repositories.FirestationsRepository;
 import com.safetynet.alerts.repositories.MedicalRecordsRepository;
@@ -13,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,27 +24,14 @@ public class FirestationService {
     private final PersonsRepository personsRepository;
     private final MedicalRecordsRepository medicalRecordsRepository;
     private final HttpServletRequest request;
+    private final PersonService personService;
 
     public PersonsInFirestationNumberResponse getPersonsFromFirestationNumber(String stationNumber) {
-        List<String> addresses = firestationsRepository.getFirestationsByStation(stationNumber);
+        List<String> addresses = firestationsRepository.getFirestationsAddressByStation(stationNumber);
         List<Person> people = personsRepository.getPeopleByAddresses(new HashSet<>(addresses));
         log.info("Request to: " + request.getRequestURI(), stationNumber);
 
-        LocalDateTime now = LocalDateTime.now();
-
-        for (Person person : people) {
-            MedicalRecord medicalRecord =
-                    medicalRecordsRepository.getMedicalRecordByFirstNameAndLastName(
-                            person.getFirstName(),
-                            person.getLastName()
-                    );
-
-            log.debug("Sort by date of birth: " + request.getRequestURI(), stationNumber);
-            int year = medicalRecord.getBirthdate().getYear() + 1900;
-            int nowYear = now.getYear();
-            person.setChild(nowYear - year <= 18);
-        }
-
+        personService.calculateChild(people);
         long childrenNumber = people.stream().filter(Person::isChild).count();
         long adultsNumber = people.stream().filter(person -> !person.isChild()).count();
 
@@ -83,7 +68,7 @@ public class FirestationService {
     }
 
     public List<String> getPhoneOfPersonToStationNumber(String firestation) {
-        List<String> addresses = firestationsRepository.getFirestationsByStation(firestation);
+        List<String> addresses = firestationsRepository.getFirestationsAddressByStation(firestation);
         log.info("Reply 200 (OK) to: " + request.getRequestURI(),firestation);
         return personsRepository.getPeoplePhoneByAddresses(new HashSet<>(addresses));
 

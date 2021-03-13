7723cfa717542687f8e05c9eeaca5dd176e99a01
Noémie Menu <71379054+noemiemenu@users.noreply.github.com>
2021-03-13 15:36:37 +0100

    Add logs (#33)

--- src/main/java/com/safetynet/alerts/controller/FirestationController.java
+++ src/main/java/com/safetynet/alerts/controller/FirestationController.java
@@ -6,38 +6,47 @@ import com.safetynet.alerts.service.FirestationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api("Gestion des Firestation ")
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class FirestationController {
 
     private final FirestationService firestationService;
+    private final HttpServletRequest request;
 
 
     @ApiOperation(value = "Retourner une liste des personnes couvertes par la caserne de pompiers est fournir un décompte du nombre d'adultes et du nombre d'enfants")
     @GetMapping("/firestations")
     public PersonsInFirestationNumberResponse getPersonsFromFirestationNumber(@RequestParam String stationNumber) {
+        log.info("Request to: " + request.getRequestURI(), stationNumber);
         return firestationService.getPersonsFromFirestationNumber(stationNumber);
     }
 
     @ApiOperation(value = "Ajout d'une nouvelle firestation")
     @PostMapping("/firestation")
     public ResponseEntity addFirestations(@RequestBody Firestation firestation) {
+        log.info("Request to: " + request.getRequestURI(), firestation);
         return firestationService.addFirestations(firestation);
     }
 
     @ApiOperation(value = "Mettre à jour une firestation")
     @PutMapping("/firestation")
     public Firestation updateFirestation(@RequestBody Firestation firestation) {
+        log.info("Request to: " + request.getRequestURI(), firestation);
         return firestationService.updateFirestation(firestation);
     }
 
     @ApiOperation(value = "Supprime une firestation")
     @DeleteMapping("/firestation")
     public ResponseEntity deleteFirestation(@RequestParam String address, @RequestParam String station) {
+        log.info("Request to: " + request.getRequestURI(), address, station);
         return firestationService.deleteFirestation(address, station);
     }
 }
--- src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Api("Gestion des MedicalRecords")
 @RestController
 @AllArgsConstructor
@@ -17,22 +19,26 @@ import org.springframework.web.bind.annotation.*;
 public class MedicalRecordController {
 
     private final MedicalRecordService medicalRecordService;
+    private final HttpServletRequest request;
 
     @ApiOperation(value = "Ajouter un dossier médical")
     @PostMapping()
     public ResponseEntity addMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
+        log.info("Request to: " + request.getRequestURI(), medicalRecord);
         return medicalRecordService.addMedicalRecord(medicalRecord);
     }
 
     @ApiOperation(value = "Mettre à jour un dossier médical")
     @PutMapping()
     public MedicalRecord updateMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
+        log.info("Request to: " + request.getRequestURI(), medicalRecord);
         return medicalRecordService.updateMedicalRecord(medicalRecord);
     }
 
     @ApiOperation(value = "Supprime un dossier médical")
     @DeleteMapping()
     public ResponseEntity deleteMedicalRecord(@RequestParam String firstName, @RequestParam String lastName) {
+        log.info("Request to: " + request.getRequestURI(), firstName, lastName);
         return medicalRecordService.deleteMedicalRecord(firstName, lastName);
     }
 
